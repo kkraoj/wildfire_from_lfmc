179ad83e47ad4d2e2288df53f0b1ef28e1dc8666
krishnakrao <kkrao.j@gmail.com>
2020-05-12 00:13:43 -0700

    modified code to investigate variogram buffer zones

--- inside_outside.py
+++ inside_outside.py
@@ -21,12 +21,14 @@ sns.set(style='ticks',font_scale = 1.5)
 
 #%% ####################################################################
 #distribution of fire per landcover type
-df = pd.read_csv(os.path.join(dir_data, "fire_collection_median_no_climate_extra_lfmc_vars.csv"))
+df = pd.read_csv(os.path.join(dir_data, "fire_collection_median_with_climate_4km_variogram.csv"))
 df.head()
 df.columns
 df.landcover.unique()
 
 #%% initialize plot
+units = {'lfmc':'(%)','vpd':'(hPa)','erc':'','ppt':r'(mm/month)'}
+axis_lims = {'lfmc':[75,125],'vpd':[15,50],'erc':[20,70],'ppt':[0,120]}
 lc_dict = { 
             50: 'Closed broadleaf\ndeciduous',
             70: 'Closed needleleaf\nevergreen',
@@ -40,6 +42,14 @@ lc_dict = {
 df = df.loc[df.landcover.isin(lc_dict.keys())]
 df['landcover'] = df.landcover.map(lc_dict)
 
+#%% histogram of buffer zone ring sizes
+# sns.set(style='ticks',font_scale = 1.)
+# fig, ax = plt.subplots(figsize = (3,1))
+
+# df.bufferDist.plot.hist(ax = ax,bins = 9)
+# ax.set_xlabel('Buffer zone radius (m)')
+
+
 
 #%% histogram of LFMC inside vs. outside
 # sub = df[['lfmc_t_1_inside','lfmc_t_1_outside','landcover']]
@@ -48,81 +58,42 @@ df['landcover'] = df.landcover.map(lc_dict)
 # sub['lfmc_t_1_outside'].plot.hist(color = 'lime',alpha = 0.5,bins = 50,ax=ax,label = 'Unaffected area')
 # ax.set_xlabel('LFMC (%)')
 # plt.legend()
+filters = (df.BurnDate>=160)&(df.area<20)
+df = df.loc[filters]
+df.shape
 
-fig, ax = plt.subplots()
-(df['lfmc_t_1_inside']-df['lfmc_t_1_outside']).plot.hist(color = 'grey',alpha = 0.5,bins = 50,ax=ax,label = 'Difference(BA-UA)')
+sns.set(style='ticks',font_scale = 1.)
+data = df['lfmc_t_1_inside']-df['lfmc_t_1_outside']
+fig, ax = plt.subplots(figsize = (3,3))
+data.plot.hist(color = 'grey',alpha = 0.5,ax=ax,label = 'Difference(BA-UA)')
 ax.set_xlabel('LFMC (%)')
-plt.legend()
-
-
-data = df['lfmc_t_2_inside']-df['lfmc_t_2_outside']
-fig, ax = plt.subplots()
-data.plot.hist(color = 'grey',alpha = 0.5,bins = 50,ax=ax,label = 't-2')
-ax.set_xlabel('LFMC (%)')
-ax.axvline(data.mean(),label = 'mean',color = 'k',linewidth = 2, linestyle = '--')
-plt.legend()
-
-data = df['lfmc_t_2_inside']-df['lfmc_t_1_inside']
-fig, ax = plt.subplots()
-data.plot.hist(color = 'grey',alpha = 0.5,bins = 50,ax=ax,label = '(t-2)-(t-1) inside')
-ax.set_xlabel('LFMC (%)')
-ax.axvline(data.mean(),label = 'mean',color = 'k',linewidth = 2, linestyle = '--')
-ax.set_xlim(-100,100)
-plt.legend()
-
-data = df['lfmc_t_2_outside']-df['lfmc_t_1_outside']
-fig, ax = plt.subplots()
-data.plot.hist(color = 'grey',alpha = 0.5,bins = 50,ax=ax,label = '(t-2)-(t-1) outside')
-ax.set_xlabel('LFMC (%)')
-ax.axvline(data.mean(),label = 'mean',color = 'k',linewidth = 2, linestyle = '--')
-ax.set_xlim(-100,100)
-plt.legend()
-
-data = df['lfmc_t_2_inside']-df['lfmc_t_1_inside'] - df['lfmc_t_2_outside']+df['lfmc_t_1_outside']
-fig, ax = plt.subplots()
-data.plot.hist(color = 'grey',alpha = 0.5,bins = 50,ax=ax,label = '(t-2)-(t-1) BA-UA')
-ax.set_xlabel('LFMC (%)')
-ax.axvline(data.mean(),label = 'mean',color = 'k',linewidth = 2, linestyle = '--')
-ax.set_xlim(-100,100)
-plt.legend()
-
-data = df['lfmc_t_1_inside']-df['lfmc_t_1_seasonal_mean_inside'] - df['lfmc_t_1_outside']+df['lfmc_t_1_seasonal_mean_outside']
-fig, ax = plt.subplots()
-data.plot.hist(color = 'grey',alpha = 0.5,bins = 50,ax=ax,label = "(t-1)' BA-UA")
-ax.set_xlabel('LFMC (%)')
-ax.axvline(data.mean(),label = 'mean',color = 'k',linewidth = 2, linestyle = '--')
-ax.set_xlim(-100,100)
+ax.axvline(data.mean(),color = 'k',linewidth = 2, label = 'mean')
 plt.legend()
 
 
 #%%
-## by landcover
-# for lc in df.landcover.unique():
-#     sub = df.loc[df.landcover==lc,['lfmc_t_1_inside','lfmc_t_1_outside','landcover']]
-#     fig, ax = plt.subplots()
-#     sub['lfmc_t_1_inside'].plot.hist(color = 'darkred',alpha = 0.5,bins = 50,ax=ax,label = 'Burned area',linewidth = 0)
-#     sub['lfmc_t_1_outside'].plot.hist(color = 'lime',alpha = 0.5,bins = 50,ax=ax,label = 'Unaffected area',linewidth = 0)
-#     ax.set_xlabel('LFMC (%)')
-#     ax.set_title('%s'%lc)
-#     plt.legend()
+# by landcover
+for lc in df.landcover.unique():
+    sub = df.loc[df.landcover==lc,['lfmc_t_1_inside','lfmc_t_1_outside','landcover']]
+    fig, ax = plt.subplots(figsize = (3,3))
+    sub['lfmc_t_1_inside'].plot.hist(color = 'darkred',alpha = 0.5,ax=ax,label = 'Burned area',linewidth = 0)
+    sub['lfmc_t_1_outside'].plot.hist(color = 'lime',alpha = 0.5,ax=ax,label = 'Unaffected area',linewidth = 0)
+    ax.axvline(sub['lfmc_t_1_inside'].mean(),color = 'darkred',linewidth = 2, label = '_nolegend_')
+    ax.axvline(sub['lfmc_t_1_outside'].mean(),color = 'darkgreen',linewidth = 2, label = '_nolegend_')
+    ax.set_xlabel('LFMC (%)')
+    ax.set_title('%s'%lc)
+    plt.legend()
 
 #%% comparing lfmc to other climate indices
-#for var in ['lfmc','vpd','ppt']:    
-#    fig, ax = plt.subplots()
-#    df['%s_t_1_inside'%var].plot.hist(color = 'darkred',alpha = 0.5,bins = 50,ax=ax,label = 'Burned area')
-#    df['%s_t_1_outside'%var].plot.hist(color = 'lime',alpha = 0.5,bins = 50,ax=ax,label = 'Unaffected area')
-#    ax.set_xlabel('%s (%%)'%var.upper())
-#    plt.legend()
-
-#%% different timescales of VPD
-# for var in ['vpd']:    
-#     for t in [2,3,4,5,6]:
-#         fig, ax = plt.subplots()
-#         df['%s_t_%1d_inside'%(var,t)].plot.hist(color = 'darkred',alpha = 0.5,bins = 50,ax=ax,label = 'Burned area')
-#         df['%s_t_%1d_outside'%(var,t)].plot.hist(color = 'lime',alpha = 0.5,bins = 50,ax=ax,label = 'Unaffected area')
-#         ax.set_xlabel('%s, summed over previous %1d months'%(var.upper(),t))
-#         plt.legend()
-
+for var in ['lfmc','vpd','ppt','erc']:    
+    cols = [col for col in df.columns if var in col]
+    fig, ax = plt.subplots(figsize = (3,3))
+    df[cols[0]].plot.hist(color = 'darkred',alpha = 0.5,ax=ax,label = 'Burned area')
+    df[cols[1]].plot.hist(color = 'lime',alpha = 0.5,ax=ax,label = 'Unaffected area')
+    # ax.axvline(data.mean(),color = 'k',linewidth = 2, label = 'mean')
+    
+    ax.set_xlabel('%s %s'%(var.upper(),units[var]))
+    plt.legend()
 
 
 #%% logit
@@ -147,41 +118,3 @@ plt.legend()
 #ax.set_yticks([0,1])
 #ax.set_yticklabels(['No fire','Fire'])
 #plt.legend()
-
-#%% variogram
-# varh = [127.4,
-# 124.9,
-# 126.2,
-# 127.3,
-# 128.9,
-# 131.4,
-# 133.3,
-# 135.5,
-# 137.9,
-# 139.5,
-# 140.7,
-# 141.4,
-# 141.9,
-# 142.5,
-# 143.3,
-# 143.9,
-# 144.6,
-# 144.9,
-# 145.3,
-# 145.7,
-# 146.1,
-# 146.4,
-# 146.8,
-# 147.1,
-# 147.5,
-# 147.9,
-# 148.2,
-# 148.6,
-# 148.8,
-# 149.1
-# ]
-# len(varh)
-# fig, ax = plt.subplots()
-# ax.plot(varh,'ok')
-# ax.set_xlabel('Radius (km)')
-# ax.set_ylabel('Variance')
