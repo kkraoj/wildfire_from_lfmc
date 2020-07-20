5d3841a0e29a679e802f506292df8b9153d7347e
krishnakrao <kkrao.j@gmail.com>
2020-07-19 20:09:28 -0700

    Fuel matched to species

--- aggregate_try.py
+++ aggregate_try.py
@@ -8,9 +8,56 @@ Created on Mon Jul 13 12:43:35 2020
 import os
 import pandas as pd
 from init import dir_data, dir_root
+from googlesearch import search
+
 
 data = pd.read_table(os.path.join(dir_root, "data","traits","TRY","P50.txt"), dtype = {"OrigUnitStr":str, "StdValue":float, "Reference":str},encoding = "latin1")
 data.drop("Unnamed: 27", axis = 1, inplace = True)
 data.OriglName.unique()
-p50s = ["psi (-MPa)","P50 (MPa)", "P50","Water potential at 50% loss of conductivity Psi_50 (MPa)","Mean ?50 (with all) (MPa)","Choat et al. 2012 reported ?50 (MPa)"]
-data.loc[data.OriglName.isin(p50s),'OrigValueStr'].astype(float).abs().multiply(-1).hist()
+p50s = ["psi (-MPa)",
+        "P50 (MPa)", 
+        "P50","Water potential at 50% loss of conductivity Psi_50 (MPa)",
+        "Mean ?50 (with all) (MPa)","Choat et al. 2012 reported ?50 (MPa)",
+        "Xylem tension at 50% loss of hydraulic conductivity (MPa)"]
+# data = data.loc[data.OriglName.isin(p50s),'OrigValueStr'].astype(float).abs().multiply(-1).hist()
+data = data.loc[data.OriglName.isin(p50s)]
+
+species = pd.Series(data.SpeciesName.unique())
+
+def map_fuel_to_species():
+    nfmd = pd.read_pickle("D:/Krishna/projects/vwc_from_radar/data/fmc_24_may_2019")
+    nfmd.head()
+    len(nfmd.fuel.unique())
+    fuels = nfmd.fuel.unique()
+    
+    nfmd.drop_duplicates(subset = ['fuel'], inplace = True)
+    nfmd.shape 
+    latlon = pd.read_csv("D:/Krishna/projects/vwc_from_radar/data/fuel_moisture/nfmd_queried_latlon.csv")
+    latlon.columns = latlon.columns.str.lower()
+    nfmd = nfmd.merge(latlon, on = "site", how = "left")
+    nfmd.dropna(inplace = True)
+    nfmd = nfmd.loc[~nfmd.fuel.isin(['1-Hour','10-Hour','100-Hour', '1000-Hour',\
+                        'Duff (DC)', '1-hour','10-hour','100-hour',\
+                        '1000-hour', 'Moss, Dead (DMC)' ])]
+    nfmd.to_excel(os.path.join(dir_root, "data","traits","TRY","dictionary_fuels_species.xlsx"))
+    
+    query = "species name Douglas-Fir"
+    my_results_list = []
+    for i in search(query,        # The query you want to run
+                    tld = 'com',  # The top level domain
+                    lang = 'en',  # The language
+                    num = 2,     # Number of results per page
+                    start = 0,    # First result to retrieve
+                    stop = 2,  # Last result to retrieve
+                    pause = 2.0,  # Lapse between HTTP requests
+                   ):
+        my_results_list.append(i)
+        print(i)
+    
+ctr = 0    
+for fuel in fuels:
+    if fuel.lower() in species.str.lower():
+        print('[INFO] Match found.')
+        ctr+=1
+
+print(ctr)
---
+++ match_fuel_to_species.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 18:12:17 2020
+
+@author: kkrao
+"""
+
+
+
+#! /usr/bin/env python3.5
+# defineterm.py
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+import html
+import codecs
+import pandas as pd
+from init import dir_data, dir_root
+import os
+
+
+# searchterm = ' '.join(sys.argv[1:])
+
+df = pd.read_excel(os.path.join(dir_root, "data","traits","TRY","dictionary_fuels_species.xlsx"))
+
+ctr = 0
+for index, row in df.iterrows():
+    fuel = row.loc["fuel"]
+    searchterm = "species name "+fuel
+    url = 'https://www.google.com/search?q=' + searchterm
+    res = requests.get(url)
+    try:
+        res.raise_for_status()
+    except Exception as exc:
+        print('error while loading page occured: ' + str(exc))
+    
+    text = html.unescape(res.text)
+    soup = BeautifulSoup(text, 'lxml')
+    # prettytext = soup.prettify()
+    
+    ## next lines are for analysis (saving raw page), you can comment them
+    # frawpage = codecs.open('D:/Krishna/projects/wildfire_from_lfmc/data/traits/TRY/rawpage3.txt', 'w', 'utf-8')
+    # frawpage.write(prettytext)
+    # frawpage.close()
+    
+    # firsttag = soup.find('h3', class_="r")
+    tags = soup.findAll("div", class_="BNeawe iBp4i AP7Wnd")
+    # <div class="BNeawe iBp4i AP7Wnd">
+    # BNeawe tAd8D AP7Wnd
+    try:
+        df.loc[index, 'species'] = tags[1].getText()
+        ctr+=1
+        print('[INFO] Found.\t Fuel name:%s.'%fuel)
+    except:
+        print('[INFO] Not found.\t Fuel name:%s.'%fuel)
+
+print('[INFO] Search Complete. Found species name for %d fuels'%ctr)
+df.to_excel(os.path.join(dir_root, "data","traits","TRY","dictionary_fuels_species.xlsx"))
